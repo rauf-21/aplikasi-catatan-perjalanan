063f61c8de5572a28e2dfcabcf1566fc39c06a78
rauf-21 <rauf21dev@gmail.com>
2022-06-30 01:05:24 +0700

    feat(page): add registration page

---
+++ pages/registration/index.tsx
@@ -0,0 +1,10 @@
+import type { NextPage } from 'next'
+import RegistrationForm from '../../components/RegistrationForm'
+
+const RegistrationPage: NextPage = () => {
+  return (
+    <RegistrationForm />
+  )
+}
+
+export default RegistrationPage
