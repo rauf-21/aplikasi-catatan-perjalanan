2c3e8b614fbb4cba3c0b34efa655acf872f6af1e
rauf-21 <rauf21dev@gmail.com>
2022-06-30 12:22:53 +0700

    refactor(page): add notification component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,24 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import '../styles/globals.css'
+import Notification from '../components/Notification'
+import useNotificationStore from '../stores/notification-store'
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const { isVisible, status, children } = useNotificationStore((state) => ({
+    isVisible: state.isVisible,
+    status: state.status,
+    children: state.children
+  }))
+
+  return (
+    <>
+      <Notification isVisible={isVisible} status={status} >
+        {children}
+      </Notification>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
