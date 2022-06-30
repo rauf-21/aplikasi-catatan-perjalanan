1da85628bcc7a8415047bec524aa44096cef7653
rauf-21 <rauf21dev@gmail.com>
2022-06-30 08:38:02 +0700

    feat(component): add Notification component

---
+++ components/Notification.tsx
@@ -0,0 +1,52 @@
+import type { NextPage } from 'next'
+import React, { useState, useEffect } from 'react'
+
+const Notification: NextPage<{
+  children: React.ReactNode,
+  status: 'success' | 'pending' | 'failed'
+}> = (props) => {
+  const { children, status } = props
+
+  const [isVisible, setIsVisible] = useState(true)
+
+  useEffect(() => {
+    if (status === 'pending') {
+      setIsVisible(true)
+    }
+
+    if (status === 'success' || status === 'failed') {
+      const timer = setTimeout(() => {
+        setIsVisible(false)
+      }, 3000)
+
+      return () => {
+        clearTimeout(timer)
+      }
+    }
+  }, [status])
+
+  const successIcon = <svg xmlns='http://www.w3.org/2000/svg' className='stroke-current flex-shrink-0 h-6 w-6' fill='none' viewBox='0 0 24 24'><path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z' /></svg>
+
+  const pendingIcon = <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' className='stroke-info flex-shrink-0 w-6 h-6'><path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z'></path></svg>
+
+  const failedIcon = <svg xmlns='http://www.w3.org/2000/svg' className='stroke-current flex-shrink-0 h-6 w-6' fill='none' viewBox='0 0 24 24'><path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z' /></svg>
+
+  return (
+    <>
+      {
+        isVisible && (
+          <div className={`alert ${status === 'success' && 'alert-success'} ${status === 'failed' && 'alert-error'} shadow-lg w-1/2 fixed top-5`}>
+            <div>
+              {status === 'success' && successIcon}
+              {status === 'pending' && pendingIcon}
+              {status === 'failed' && failedIcon}
+              <span>{children}</span>
+            </div>
+          </div>
+        )
+      }
+    </>
+  )
+}
+
+export default Notification
