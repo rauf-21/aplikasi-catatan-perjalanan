78af157736641d995b0e98f51d1e405a197f64c5
rauf-21 <rauf21dev@gmail.com>
2022-06-30 13:09:38 +0700

    refactor(component): add login handler

--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -1,19 +1,50 @@
 import type { NextPage } from 'next'
 import type { FormEventHandler } from 'react'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 import { useState } from 'react'
 
+import useNotificationStore from '../stores/notification-store'
+
 const LoginForm: NextPage = () => {
-
+  const router = useRouter()
   const [nik, setNik] = useState('')
+  const [nikErrors, setNikErrors] = useState<string[]>([])
 
-  const loginHandler: FormEventHandler<HTMLFormElement> = (e) => {
+  const showNotification = useNotificationStore((state) => state.showNotification)
+
+  const loginHandler: FormEventHandler<HTMLFormElement> = async (e) => {
     e.preventDefault()
+
+    showNotification('pending', 'Mengirim data...')
+
+    setNikErrors([])
+
+    const response = await fetch(`/api/user/${nik}`)
+    const { success, errors, data } = await response.json()
+
+    console.log(data)
+
+    if (!success) {
+      showNotification('error', 'Login gagal!')
+
+      errors.forEach((error: string) => {
+        if (error.includes('nik')) {
+          setNikErrors(value => [...value, error])
+        }
+      })
+
+      return
+    }
+
+    showNotification('success', 'Login berhasil')
+
+    router.push('/dashboard')
   }
 
   return (
     <section className='w-full'>
-      <form onSubmit={loginHandler} className='md:w-1/2 mt-10 mx-auto p-10 flex flex-col bg-base-200 md:rounded-lg'>
+      <form onSubmit={loginHandler} className='md:w-1/2 mt-10 mx-auto p-10 flex flex-col gap-5 bg-base-200 md:rounded-lg'>
         <h1 className='w-full mb-10 text-4xl text-center font-bold'>
           <span>Selamat Datang di</span>
           <br />
@@ -21,8 +52,17 @@ const LoginForm: NextPage = () => {
         </h1>
         <label className='input-group input-group-vertical'>
           <span className='py-2'>NIK</span>
-          <input type='number' placeholder='Masukkan NIK kamu disini...' className='w-full input input-bordered' required />
+          <input type='number' placeholder='Masukkan NIK kamu disini...' className='w-full input input-bordered' required onChange={(e) => setNik(e.currentTarget.value)} />
         </label>
+        {
+          nikErrors.length > 0 && <ul className='mb-4 list-disc pl-5'>
+            {
+              nikErrors.map((error, index) => (
+                <li className='text-error capitalize' key={index}>{error}</li>
+              ))
+            }
+          </ul>
+        }
         <button type='submit' className='my-5 btn btn-primary'>Masuk</button>
         <p className='my-5 text-center'>
           Belum punya akun? Silahkan daftar&nbsp;
