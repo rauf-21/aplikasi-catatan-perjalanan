01d3edd3338796ac67cc3c7feb28b6b812ea0794
rauf-21 <rauf21dev@gmail.com>
2022-07-04 09:21:16 +0700

    refactor(page): wrap page in MainLayout

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,17 +1,14 @@
 import type { NextPage } from 'next'
 
-import NavigationBar from '../../components/NavigationBar'
-import Profile from '../../components/Profile'
 import DashboardMenu from '../../components/DashboardMenu'
+import MainLayout from '../../layouts/MainLayout'
 
 const DashboardPage: NextPage = () => {
   return (
     <>
-      <NavigationBar />
-      <section className='w-full px-5 py-10 flex flex-row gap-10'>
-        <Profile />
+      <MainLayout>
         <DashboardMenu />
-      </section>
+      </MainLayout>
     </>
   )
 }
